5c48b0ac9d3d6c966e32103bcdfa15c6c94b5595
Csecskedi Csaba <csaba.csecskedi@possible.com>
2015-06-16 15:48:57 +0200

    Collision & test

--- src/game.js
+++ src/game.js
@@ -1,121 +1,119 @@
-var Game = (function() {
-	
-	var frame = 0;
-		motors = [];
-	
-	function Motor(x, y, v) {
-		this.x = x;
-		this.y = y;
-		this.v = v;
-		this.speed = 1;
-		this.data = [];
-		this.add();
-	}
-	
-	Motor.UP = 0;
-	Motor.RIGHT = 1;
-	Motor.DOWN = 2;
-	Motor.LEFT = 3;
-	
-	Motor.prototype.add = function() {
-		this.data.push([this.x, this.y, this.v]);
-	};
-	
-	Motor.prototype.move = function() {
-		switch (this.v) {
-			case Motor.LEFT:
-				this.x -= this.speed;
-				break;
-			case Motor.RIGHT:
-				this.x += this.speed;
-				break;
-			case Motor.UP:
-				this.y -= this.speed;
-				break;
-			case Motor.DOWN:
-				this.y += this.speed;
-				break;
-		}
-	};
-	
-	Motor.prototype.turn = function (to){
-		switch (to) {
-			case Motor.LEFT:
-				if (--this.v < Motor.UP) this.v = Motor.LEFT;
-				this.add();
-				break;
-			case Motor.RIGHT:
-				if (++this.v > Motor.LEFT) this.v = Motor.UP;
-				this.add();
-				break;
-		}
-	};
+var frame = 0;
+    motors = [];
 
-    Motor.prototype.check = function(x1, y1, x2, y2) {
-        var i = this.data.length - 1,
-            x = this.data[i][0],
-            y = this.data[i][1],
-            a1 = y2 - y1,
-            b1 = x1 - x2,
-            c1 = a1 * x1 + b1 * y1,
-            a2 = y - this.y,
-            b2 = this.x - x,
-            c2 = a2 * this.x + b2 * this.y;
-            d = a1 * b2 - a2 * b1;
-        if (d == 0) {
-            return false;
-        } else {
-            x = (b2 * c1 - b1 * c2) / d;
-            y = (a1 * c2 - a2 * c1) / d;
-            console.log([x, y]);
-            x1 -= x;
-            x2 -= x;
-            y1 -= y;
-            y2 -= y;
-            return x1 <= 0 && x2 >= 0 || x2 <= 0 && x1 >= 0 || y1 <= 0 && y2 >= 0 || y2 <= 0 && y1 >= 0;
-        }
-    };
+function Motor(x, y, v) {
+    this.x = x;
+    this.y = y;
+    this.v = v;
+    this.speed = 1;
+    this.data = [];
+    this.add();
+}
 
-	function add(x, y, v) {
-		var motor = new Motor(x, y, v);
-		motors.push(motor);
-		return motor;
-	}
+Motor.UP = 0;
+Motor.RIGHT = 1;
+Motor.DOWN = 2;
+Motor.LEFT = 3;
 
-    function check(motor)
-    {
-        var result = false;
-        motors.forEach(function(other) {
-            var x = other.x,
-                y = other.y;
-            other.data.forEach(function(item) {
-                if (motor.check(x, y, item[0], item[1])) {
-                    result = true;
-                }
-                x = item[0];
-                y = item[1];
-            });
-        });
-        return result;
+Motor.prototype.add = function() {
+    this.data.push([this.x, this.y, this.v]);
+};
+
+Motor.prototype.move = function() {
+    switch (this.v) {
+        case Motor.LEFT:
+            this.x -= this.speed;
+            break;
+        case Motor.RIGHT:
+            this.x += this.speed;
+            break;
+        case Motor.UP:
+            this.y -= this.speed;
+            break;
+        case Motor.DOWN:
+            this.y += this.speed;
+            break;
     }
+};
 
-	function get() {
-		return motors;
-	}
+Motor.prototype.turn = function (to){
+    switch (to) {
+        case Motor.LEFT:
+            if (--this.v < Motor.UP) this.v = Motor.LEFT;
+            this.add();
+            break;
+        case Motor.RIGHT:
+            if (++this.v > Motor.LEFT) this.v = Motor.UP;
+            this.add();
+            break;
+    }
+};
 
-	function run() {
-		frame++;
-		motors.forEach(function(motor) {
-            if (!check(motor)) {
-                motor.move();
+Motor.prototype.inline = function(x, y, x1, y1, x2, y2) {
+    x1 -= x;
+    x2 -= x;
+    y1 -= y;
+    y2 -= y;
+    return x1 <= 0 && x2 >= 0 || x2 <= 0 && x1 >= 0 || y1 <= 0 && y2 >= 0 || y2 <= 0 && y1 >= 0;
+};
+
+Motor.prototype.check = function(x1, y1, x2, y2) {
+    var i = this.data.length - 1,
+        x = this.data[i][0],
+        y = this.data[i][1],
+        d = ((x2 - x1) * (y - this.y)) - ((y2 - y1) * (x - this.x)),
+        n1 = ((y1 - this.y) * (x - this.x)) - ((x1 - this.x) * (y - this.y)),
+        n2 = ((y1 - this.y) * (x2 - x1)) - ((x1 - this.x) * (y2 - y1)),
+        r,
+        s;
+    if (d == 0) {
+        return n1 == 0 && n2 == 0;
+    }
+    r = n1 / d;
+    s = n2 / d;
+    return (r >= 0 && r <= 1) && (s >= 0 && s <= 1);
+};
+
+function add(x, y, v) {
+    var motor = new Motor(x, y, v);
+    motors.push(motor);
+    return motor;
+}
+
+function check(motor)
+{
+    var result = false;
+    motors.forEach(function(other) {
+        var x = other.x,
+            y = other.y;
+        other.data.forEach(function(item) {
+            if (motor.check(x, y, item[0], item[1])) {
+                result = true;
             }
-		});
-	}
+            x = item[0];
+            y = item[1];
+        });
+    });
+    return result;
+}
 
-	return {
-		get: get,
-		add: add,
-		run: run
-	};
-	
-})();
+function get() {
+    return motors;
+}
+
+function run() {
+    frame++;
+    motors.forEach(function(motor) {
+        if (!check(motor)) {
+            motor.move();
+        }
+    });
+}
+
+module.exports =  {
+    Motor: Motor,
+    check: check,
+    get: get,
+    add: add,
+    run: run
+};
---
+++ test/game.js
@@ -0,0 +1,26 @@
+var assert = require('assert'),
+    game = require('../src/game.js');
+
+var motor = new game.Motor(0, 0, 0);
+
+motor.data.push([40, 0, 3]);
+
+describe('game.Motor', function() {
+    describe('check', function() {
+        it('return true when crossing', function() {
+            assert.equal(true, motor.check(10, 10, 10, -10));
+        });
+        it('return true when contacting', function() {
+            assert.equal(true, motor.check(0, 10, 0, -10));
+        });
+        it('return false when not crossing', function() {
+            assert.equal(false, motor.check(50, 10, 50, -10));
+        });
+        it('return false when paralel', function() {
+            assert.equal(false, motor.check(0, 10, 10, 10));
+        });
+        it('return true when fade', function() {
+            assert.equal(true, motor.check(0, 0, 10, 0));
+        });
+    });
+});
