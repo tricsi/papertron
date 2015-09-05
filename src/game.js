c7b274e81926bf0ba4f4801e45ff839b818cf13f
csecskedics <csaba.csecskedi@carnationgroup.com>
2015-09-05 16:10:45 +0200

    timer

--- src/game.js
+++ src/game.js
@@ -189,7 +189,7 @@ global.exports = (function () {
 	 * @constructor
 	 */
     function Match(mode, map) {
-        this.timer = 35; // Snapshot time
+        this.timer = 40; // Snapshot time
         this.distance = 80; // Wall distance
         this.start = new Date().getTime() + 3000; // Start time
         this.motors = []; // Motors
